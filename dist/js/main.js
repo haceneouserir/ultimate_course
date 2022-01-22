16c38b2cfd80c13cf7c08291a96e9e4819acf8cd
Hacene Ouserir <haceneouserir@gmail.com>
2022-01-22 22:14:54 +0100

    Added...

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,14 @@
 $(function() {
   'use strict';
   $('.toggle-sidebar').on('click', function() {
-    $('.content-area, .sidebar').toggleClass('no-sidebar')
+    $('.content-area, .sidebar').toggleClass('no-sidebar');
+  })
+  // Toggle submenu
+  $('.toggle-submenu').on('click', function() {
+    $(this).find('.fa-angle-right').toggleClass('down');
+    $(this)
+      .next('.child-links')
+      .slideToggle();
   })
 });
 const map = new Map();
