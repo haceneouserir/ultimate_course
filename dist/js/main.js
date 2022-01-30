3e50865d2f0265de0a1c7631e48592247c42fff0
Hacene Ouserir <haceneouserir@gmail.com>
2022-01-30 08:23:51 +0100

    Added...

--- dist/js/main.js
+++ dist/js/main.js
@@ -24,6 +24,16 @@ $(function() {
     $(this).find('i').toggleClass('fa-spin');
     $(this).parent().toggleClass('hide-settings');
   });
+
+  // Switch colors themes
+  var themesClasses = [];
+  $('.color-options li').each(function() {
+    themesClasses.push($(this).data('theme'))
+  });
+  $('.color-options li').on('click', function() {
+    $(this).addClass('active').siblings().removeClass('active');
+    $('body').removeClass(themesClasses.join(' ')).addClass($(this).data('theme'));
+  });
 });
 
 /* Get the documentElement (<html>) to display the page in fullscreen */
